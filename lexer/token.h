5d23ca9054b4e6989fe75c7a5683230e9c74c2ba
certifiedUnc <ttakshatt@gmail.com>
2026-08-12 23:47:20 +0530

    added variable, number ast expr class

--- lexer/token.h
+++ lexer/token.h
@@ -2,11 +2,11 @@
 #define __TOKEN_H__
 
 enum Token{
-    tok_eof = -1;
-    tok_def = -2;
-    tok_extern = -3;
-    tok_identifier = -4;
-    tok_number = -5;
+    tok_eof = -1,
+    tok_def = -2,
+    tok_extern = -3,
+    tok_identifier = -4,
+    tok_number = -5,
 };
 
 static std::string IdentifierStr;
